29c3c0d27a5ecd30e5bd5fa2dd90b317bb30c39d
Victor <47831936+VictorEgret@users.noreply.github.com>
2022-09-15 01:13:00 +0200

    Update

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -12,39 +12,49 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include "libft.h"
 
 void	handle_conv(char c, va_list args)
 {
-	if (c == 'c')
-		ft_putchar(va_arg(args, char));
-	else if (c == 'd')
-		ft_putnbr(va_arg(args, int));
-	else if (c == '%')
-		ft_putchar('%');
-	else if (c == 's')
-		ft_putstr(va_arg(args, char *));
-	else if (c == '')
+	//if (c == 'c')
+	//	ft_putchar(va_arg(args, char));
+	//else if (c == 'd' || c == 'i')
+	//	ft_putnbr(va_arg(args, int));
+	//else if (c == '%')
+	//	ft_putchar('%');
+	//else if (c == 's')
+	//	ft_putstr(va_arg(args, char *));
+	//else if (c == 't')
+	//	ft_putnbr(va_arg(args, int));
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *format, ...)
 {
-	int i;
-	va_list args;
+	int				i;
+	unsigned int	count;
+	va_list			args;
 
 	i = 0;
-	va_start(args, str);
-	while (str[i] != '\0')
+	count = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
-		if (str[i] == '%' && str[i + 1] != '\0')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			handle_conv(str[i + 1], args);
+			count += handle_conv(format[i + 1], args);
 			i++;
 		}
 		else
-			ft_putchar(str[i]);
+			//count += ft_putchar(format[i]);
 		i++;
 	}
 	va_end(args);
+	return (count);
+}
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", 0x2A);
 	return 0;
 }
