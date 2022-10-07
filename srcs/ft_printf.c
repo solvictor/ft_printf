12e9bc802e2c1b657ba172953d342cdaf25d46a7
Victor <47831936+VictorEgret@users.noreply.github.com>
2022-10-08 01:39:50 +0200

    Flags implementation WIP

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -12,10 +12,45 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "ft_printf.h"
+
+typedef struct	s_flag
+{
+	const char	*type;
+	int			value;
+}				t_flag;
+
+int	ft_atoi(const char *s, int *i)
+{
+	int	r;
+
+	r = 0;
+	while (s[*i] >= '0' && s[*i] <= '9')
+	{
+		r = r * 10 + s[*i] - '0';
+		(*i)++;
+	}
+	return (r);
+}
+
+int	handle_flags(const char *s)
+{
+	t_flag	*flag;
+	int		i;
+
+	if (*s != '-' && *s != '0')
+		return (0);
+	i = 0;
+	flag = malloc(sizeof(t_flag));
+	flag->type = s;
+	flag->value = ft_atoi(s + 1, &i);
+	return (i + 1);
+}
 
 int	handle_conv(const char *c, va_list args)
 {
+	c += handle_flags(c);
 	if (*c == 'c')
 		return (ft_putchar(va_arg(args, int)));
 	if (*c == 's')
@@ -43,9 +78,9 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	count = 0;
 	va_start(args, format);
-	while (format[i] != '\0')
+	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%' && format[i + 1])
 		{
 			i++;
 			count += handle_conv(format + i, args);
